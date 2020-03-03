6a2aef1aa73d99d484ffbb6db2d4f99f17feee16
Tanish arora <arora.tanish97@ucr-secure-29-10-13-166-132.wnet.ucr.edu>
2020-03-02 16:41:01 -0800

    hello world veerey

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Hor paji ki hal chall</Text>
+      <Text> Sab theek?</Text> 
     </View>
   );
 }
